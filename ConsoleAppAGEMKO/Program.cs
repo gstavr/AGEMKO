d499ed953bf413c20f5cbdb4b815072f8b7a25c7
gstavr <gstavr1988@gmail.com>
2018-12-13 21:49:24 +0200

    .

--- ConsoleAppAGEMKO/Program.cs
+++ ConsoleAppAGEMKO/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace ConsoleAppAGEMKO
 {
@@ -6,7 +8,46 @@ namespace ConsoleAppAGEMKO
     {
         static void Main(string[] args)
         {
+
+            testc();
             Console.WriteLine("Hello World!");
         }
+
+
+
+
+        private static void testc()
+        {
+
+            //MySqlConnection conn;
+            string myConnectionString = "server=127.0.0.1;uid=root;pwd=giorgos5756;database=mydb";
+
+            //try
+            //{
+            //    conn = new MySql.Data.MySqlClient.MySqlConnection();
+            //    conn.ConnectionString = myConnectionString;
+            //    conn.Open();
+            //}
+            //catch (MySql.Data.MySqlClient.MySqlException ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+
+            DataTable dt = new DataTable();
+            using (MySqlConnection conn = new MySqlConnection(myConnectionString))
+            {
+                //conn.ConnectionString = myConnectionString;
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM mydb.individualcategory;", conn);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                conn.Close();
+            }
+           
+        }
+
+
+        
     }
+    
 }
