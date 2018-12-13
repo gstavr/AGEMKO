77db4bb57e768c562c3de1f48eadb747f1e4a93f
gstavr <gstavr1988@gmail.com>
2018-12-13 20:26:11 +0200

    Add project files.

---
+++ ConsoleAppAGEMKO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppAGEMKO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
