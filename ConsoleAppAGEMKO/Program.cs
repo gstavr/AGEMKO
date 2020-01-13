afd7531829a11422e4db70f69878fa4aa0efeda1
gstavr1988@gmail.com <giorgos5756>
2020-01-13 10:14:17 +0200

    .

--- ConsoleAppAGEMKO/Program.cs
+++ ConsoleAppAGEMKO/Program.cs
@@ -22,8 +22,6 @@ namespace ConsoleAppAGEMKO
             //ReadExcelFile();
             ReadExcelFileForSqlScript();
             CreateScript();
-            //testc();
-            Console.WriteLine("Hello World!");
         }
 
         private static void ReadExcelFileForSqlScript()
@@ -556,38 +554,6 @@ namespace ConsoleAppAGEMKO
 
             }
         }
-
-        private static void testc()
-        {
-
-            //MySqlConnection conn;
-            string myConnectionString = "server=127.0.0.1;uid=root;pwd=giorgos5756;database=mydb";
-
-            //try
-            //{
-            //    conn = new MySql.Data.MySqlClient.MySqlConnection();
-            //    conn.ConnectionString = myConnectionString;
-            //    conn.Open();
-            //}
-            //catch (MySql.Data.MySqlClient.MySqlException ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
-
-            DataTable dt = new DataTable();
-            //using (MySqlConnection conn = new MySqlConnection(myConnectionString))
-            //{
-            //    //conn.ConnectionString = myConnectionString;
-            //    conn.Open();
-            //    MySqlCommand cmd = new MySqlCommand("SELECT * FROM mydb.individualcategory;", conn);
-            //    MySqlDataReader reader = cmd.ExecuteReader();
-            //    dt.Load(reader);
-            //    conn.Close();
-            //}
-
-        }
-
-
 
     }
 
